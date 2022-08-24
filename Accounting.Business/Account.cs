003200017bc18528f3632a565d3b18f73ad32a28
Kiana <83605581+kianakermani@users.noreply.github.com>
2022-08-24 09:53:58 +0430

    Add files via upload

---
+++ Accounting.Business/Account.cs
@@ -0,0 +1,30 @@
+﻿using Accounting.DataLayer.Context;
+using Accounting.ViewModels.Accounting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounting.Business
+{
+    public class Account
+    {
+        public static ReportViewModel ReportFormMain()
+        {
+            ReportViewModel rp = new ReportViewModel();
+            using (UnitOfWork db = new UnitOfWork())
+            {
+                DateTime startDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 01);
+                DateTime EndDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 30);
+                var recieve = db.AccountingRepository.Get(a => a.TypeID == 1 && a.DateTime >= startDate && a.DateTime <= EndDate).Select(a => a.Amount).ToList();
+                var pay = db.AccountingRepository.Get(a => a.TypeID == 2 && a.DateTime >= startDate && a.DateTime <= EndDate).Select(a => a.Amount).ToList();
+
+                //rp.Recieve = recieve.Sum();
+                //rp.Pay = pay.Sum();
+                //rp.Remaining = (recieve.Sum() - pay.Sum());
+            }
+            return rp;
+        }
+    }
+}
